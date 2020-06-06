176f08c0f36d5fe49a4c438e8d26a955d1dfb931
YDLin <32437011+leandon@users.noreply.github.com>
2020-06-06 15:53:03 +0800

    更新邮件发送模板
    更新邮件发送模板

--- sendEmail.py
+++ sendEmail.py
@@ -1,23 +1,23 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
-
+ 
 def email(csdn_article_title,csdn_article_url):
-
-    sender = '913397230@qq.com'#填写发件人
-    pwd = 'wlphpcnmjpcvbbfi'#登录密码
-    receivers = ['yongdonglin@126.com']#填写收件人
-
+ 
+    sender = ' '#填写发件人
+    pwd = ' '#登录密码，是邮件第三方登录的那个密码
+    receivers = [' ']#填写收件人
+ 
     mainText="网站有内容更新,更新题目为："+csdn_article_title+"更新网址为："+csdn_article_url
     message = MIMEText(mainText,"plain",'utf-8')
     # 三个参数：第一个为文本内容，第二个为plain设置文本格式，第三个为utf-8设置编码
-    message ['From'] = "小道消息 <913397230@qq.com>"
-    message ['To'] = "小林子 <yongdonglin@126.com>"
-
+    message ['From'] = "XXX <XX@qq.com>"
+    message ['To'] = "XXX <XX@126.com>"
+ 
     subject = "CSDN网站有内容更新"
     #邮件主题
     message["Subject"] = subject
-
+ 
     try:
         # 使用非本地服务器，需要建立ssl连接
         smtpObj = smtplib.SMTP_SSL("smtp.qq.com",465)
